3603cb219a65753b5c08e1a5a4f613acdb3b5ccf
Hossein Shafiei <hshafiei374@gmail.com>
2020-08-16 10:03:24 +0430

    create composer.json BitrahServiceProvider

---
+++ src/BitrahServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hshafiei374\Bitrah;
+
+use Illuminate\Support\ServiceProvider;
+
+class BitrahServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
